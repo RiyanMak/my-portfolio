fa8f265901a28691eb9a1a79d1ece942093d9881
Riyan Maknojia <riyanmak123@gmail.com>
2024-06-02 19:00:56 -0500

    feat: added header component

---
+++ src/Components/Header.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Header = () => {
+    return(
+        <header>
+            <nav>
+                <div className="logo">{
+                    <a href="#">My Portfolio</a>
+                }</div>
+            </nav>
+        </header>
+        
+    );
+};
+
+export default Header;
